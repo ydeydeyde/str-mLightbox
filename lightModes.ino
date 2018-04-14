e189a4601782192158cb97b1a5f5f63105bef4f3
ydeydeyde <35302563+ydeydeyde@users.noreply.github.com>
2018-04-14 17:55:07 +0200

    Update lightModes.ino
    Structure coming together. Two toggle switch functions added.

--- lightModes.ino
+++ lightModes.ino
@@ -3,23 +3,46 @@
 #define SOUND_PIN 9
 #define TOGGLE_PIN 6
 
+// Incremental variable
+int colorInc; 
+
+// Strip with 18 pixels
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(18, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+int toggleStatus = LOW;
 int soundDetect = HIGH;
 
 void setup() {
   strip.clear();
   strip.begin();
-  
-  // Input from sound detection module
+ // Serial.begin(115200);
+ // Input from sound detection module
   pinMode(SOUND_PIN, INPUT); 
+  pinMode(TOGGLE_PIN, INPUT);
 }
 
 void loop() {
+  toggleStatus = digitalRead(TOGGLE_PIN);
   soundDetect = digitalRead (SOUND_PIN); 
-    // If we hear sound
+  // If we hear sound
   //  if (soundDetect == LOW) {
-  if (soundDetect == 1) {
+  if (toggleStatus == 1) {
+    chillMode(); 
+  } else { 
+    bassMode();
+  }
+}
+
+void chillMode() {
+  uint16_t i;
+  for (j = 255; j > 0; j--) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, j, j, j);
+    }
+}
+
+void bassMode() {
+}  if (soundDetect == 1) {
      fadeDown(); 
     } else {
       uint16_t i;
